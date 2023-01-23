0f19a99e22a49fa9e1e00539cf3cd1854b00508e
mehtapratik <mehta.pratik@live.com>
2023-01-23 11:29:14 -0500

    video demo page.

--- .eleventy.js
+++ .eleventy.js
@@ -1,19 +1,42 @@
 module.exports = function (config) {
-
   config.addFilter("formatUTCDate", function (value) {
-    var months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
-    var days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
-    return `${days[value.getUTCDay()]}, ${months[value.getUTCMonth()]} ${value.getUTCDate()}, ${value.getUTCFullYear()}`;
+    var months = [
+      "January",
+      "February",
+      "March",
+      "April",
+      "May",
+      "June",
+      "July",
+      "August",
+      "September",
+      "October",
+      "November",
+      "December",
+    ];
+    var days = [
+      "Sunday",
+      "Monday",
+      "Tuesday",
+      "Wednesday",
+      "Thursday",
+      "Friday",
+      "Saturday",
+    ];
+    return `${
+      days[value.getUTCDay()]
+    }, ${months[value.getUTCMonth()]} ${value.getUTCDate()}, ${value.getUTCFullYear()}`;
   });
   config.addPassthroughCopy("src/assets/fonts");
   config.addPassthroughCopy("src/assets/images");
+  config.addPassthroughCopy("src/assets/videos");
   config.addPassthroughCopy("src/assets/css/*.css");
   config.addPassthroughCopy("src/assets/css/*.map");
 
   return {
     dir: {
       input: "src",
-      output: "dist"
-    }
+      output: "dist",
+    },
   };
 };
--- src/posts/video.md
+++ src/posts/video.md
@@ -1,3 +1,14 @@
+---
+layout: post.njk
+lastUpdated: 2023-01-23
+date: 2023-01-23
+tags:
+  - post
+  - series
+title: Monthly Business Snapshot
+blurb: Monthly Business Snapshot
+---
+
 <video autoPlay muted>
    <source src="/assets/videos/all-together-now.mp4"/>
 </video>
